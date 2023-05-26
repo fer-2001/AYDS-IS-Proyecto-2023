0221a8b4b572986683516bd791cd68777a008c13
Urquizacarlos <alfredocarlos123321@gmail.com>
2023-05-26 17:46:27 -0300

    No permitir la entrega de casillas en blanco

--- miapp/models/user.rb
+++ miapp/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     validates_uniqueness_of :name
+    validates :name, presence: true
+    validates :pass, presence: true
     has_one :progress
     has_and_belongs_to_many :question
     has_many :suggestions
