8efc05df5e19e2dc02d4fae63c33ff8383aa0ec0
Urquizacarlos <alfredocarlos123321@gmail.com>
2023-06-02 12:17:50 -0300

    Carga de algunas preguntas

--- miapp/db/seeds.rb
+++ miapp/db/seeds.rb
@@ -34,28 +34,43 @@ generate_question('¿Cual es la mayor Racha de invicto que tuvo la escaloneta?',
                     { content: '29', correct: false },
                     { content: '27', correct: false },
                     { content: '28', correct: false },
-                    { content: '30', correct: true }
+                    { content: '36', correct: true }
                   ], 5, 15, 'COMPLETAR')
 
-generate_question('¿Que fecha la Scaloneta gano la copa America?', [
-                    { content: '10/10/021', correct: false },
-                    { content: '10/6/021', correct: false },
-                    { content: '10/7/021', correct: false },
-                    { content: '10/8/021', correct: true }
+generate_question('¿Que fecha la Scaloneta gano la Copa America?', [
+                    { content: '10/10/2021', correct: false },
+                    { content: '10/6/2021', correct: false },
+                    { content: '10/7/2021', correct: true },
+                    { content: '10/8/2021', correct: false }
                   ], 6, 15, 'COMPLETAR')
 
 generate_question('¿Despues de cuantos años Argentina gano nuevamente el mundial?', [
                     { content: '15', correct: false },
                     { content: '20', correct: false },
-                    { content: '28', correct: false },
-                    { content: '29', correct: true }
+                    { content: '36', correct: true },
+                    { content: '29', correct: false }
                   ], 7, 15, 'COMPLETAR')
 
-generate_question('¿Despues de cuantos años Argentina gano nuevamente el mundial?', [
+generate_question('¿Cuantos goles hizo Argentina en la Copa America?', [
                     { content: '15', correct: false },
-                    { content: '20', correct: false },
-                    { content: '28', correct: false },
-                    { content: '29', correct: true }
+                    { content: '12', correct: true },
+                    { content: '25', correct: false },
+                    { content: '18', correct: false }
                   ], 8, 15, 'COMPLETAR')
+
+generate_question('¿Quien marco el gol ganador en la final Copa America?', [
+                    { content: 'Lionel Messi', correct: false },
+                    { content: 'Angel Di Maria', correct: true },
+                    { content: 'Julian Alvarez', correct: false },
+                    { content: 'Emiliano Martinez', correct: false }
+                  ], 9, 15, 'COMPLETAR')
+
+generate_question('¿Cuantos años tenia Scaloni durante el mundial 2022?', [
+                    { content: '44', correct: true },
+                    { content: '46', correct: false },
+                    { content: '43', correct: false },
+                    { content: '37', correct: false }
+                  ], 10, 15, 'De los 32 entrenadores que dirigen en el Mundial, Scaloni es el menor a sus 
+                  44 años por lo cual es el director técnico más joven de la Copa del Mundo 2022')
                   
               
