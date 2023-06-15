d3070d2ad388e3e140be442b17ea514aea2d36dc
MaicolBM <maaicolmorenooo@gmail.com>
2023-06-14 23:58:24 -0300

    Correccion de tabla progreso y semilla

---
+++ miapp/db/migrate/20230511142344_create_progresses_table.rb
@@ -0,0 +1,15 @@
+class CreateProgressesTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :progresses do |t|
+      t.belongs_to :user
+      t.integer :current_question, default: 0
+      t.integer :correct_answers, default: 0
+      t.integer :incorrect_answers, default: 0
+      t.integer :lose_points, default: 0
+      t.integer :points, default: 0
+      t.integer :question_index
+      #t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- miapp/db/seeds.rb
+++ miapp/db/seeds.rb
@@ -15,41 +15,41 @@ generate_question('¿Cuántos goles marcó Lionel Messi durante el Mundial 2022?
                     #{Messi metio 7 goles en el mundial 2022, salio 2do mayor goleador del mundial}
                   ], 3, 10, 'Messi metio 7 goles en el mundial 2022, salio 2do mayor goleador del mundial')
 
-generate_question('¿Cuál fue el resultado del partido de Argentina contra Francia en los octavos de final?', [
-                    { content: 'Empate 1-1', correct: false },
-                    { content: 'Victoria de Argentina 2-1', correct: true },
-                    { content: 'Derrota de Argentina 0-2', correct: false },
+generate_question('¿Cuál fue el resultado del partido de Argentina contra Francia en la final del mundial 2022?', [
+                    { content: 'Empate 3-3', correct: true },
+                    { content: 'Victoria de Argentina 2-1', correct: false },
+                    { content: 'Empate 2-2', correct: false },
                     { content: 'Derrota de Argentina 1-3', correct: false }
                     #{El Argentina- Francia en octavos resulto en la victoria de argentina 2-1, }
-                  ], 2, 15, 'El Argentina- Francia en octavos resulto en la victoria de argentina 2-1')
+                  ], 2, 15, 'Luego de esto Argentina se consagro CAMPEON MUNDIAL ganandole 4-2 a Francia en penales.')
 
 generate_question('¿Quien fue el DT de la seleccion durante el mundial 2022?', [
                     { content: 'Maradona', correct: false },
                     { content: 'Sampaoli', correct: false },
                     { content: 'Mascherano', correct: false },
                     { content: 'Scaloni', correct: true }
-                  ], 4, 15, 'COMPLETAR')
- 
+                  ], 4, 15, 'Scaloni fue el DT mas joven del mundial 2022 con 44 años de edad')
+
 generate_question('¿Cual es la mayor Racha de invicto que tuvo la escaloneta?', [
                     { content: '29', correct: false },
                     { content: '27', correct: false },
                     { content: '28', correct: false },
                     { content: '36', correct: true }
-                  ], 5, 15, 'COMPLETAR')
+                  ], 5, 15, 'El único partido que perdió Argentina fue contra Arabia Saudita')
 
 generate_question('¿Que fecha la Scaloneta gano la Copa America?', [
                     { content: '10/10/2021', correct: false },
                     { content: '10/6/2021', correct: false },
                     { content: '10/7/2021', correct: true },
                     { content: '10/8/2021', correct: false }
-                  ], 6, 15, 'COMPLETAR')
+                  ], 6, 15, 'Argentina volvió a ganar la Copa América después de 28 años, tras derrotar a Brasil')
 
 generate_question('¿Despues de cuantos años Argentina gano nuevamente el mundial?', [
                     { content: '15', correct: false },
                     { content: '20', correct: false },
                     { content: '36', correct: true },
                     { content: '29', correct: false }
-                  ], 7, 15, 'COMPLETAR')
+                  ], 7, 15, 'El ultimo mundial ganado habia sido en 1986 (Mexico) , con Carlos Billardo como DT')
 
 generate_question('¿Cuantos goles hizo Argentina en la Copa America?', [
                     { content: '15', correct: false },
@@ -63,14 +63,14 @@ generate_question('¿Quien marco el gol ganador en la final Copa America?', [
                     { content: 'Angel Di Maria', correct: true },
                     { content: 'Julian Alvarez', correct: false },
                     { content: 'Emiliano Martinez', correct: false }
-                  ], 9, 15, 'COMPLETAR')
+                  ], 9, 15, 'Messi tambien fue el maximo goleador de dicho campeonato con un total de 7 goles')
 
 generate_question('¿Cuantos años tenia Scaloni durante el mundial 2022?', [
                     { content: '44', correct: true },
                     { content: '46', correct: false },
                     { content: '43', correct: false },
                     { content: '37', correct: false }
-                  ], 10, 15, 'De los 32 entrenadores que dirigen en el Mundial, Scaloni es el menor a sus 
+                  ], 10, 15, 'De los 32 entrenadores que dirigen en el Mundial, Scaloni es el menor a sus
                   44 años por lo cual es el director técnico más joven de la Copa del Mundo 2022')
-                  
-              
+
+
